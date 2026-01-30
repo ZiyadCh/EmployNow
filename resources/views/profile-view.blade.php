80556c95b94ed4d53fdd8001facfa065f09151d8
ZiyadCh <chetoziyad@gmail.com>
2026-01-30 16:39:20 +0100

    image is uploaded and saved but not displayed

--- resources/views/profile-view.blade.php
+++ resources/views/profile-view.blade.php
@@ -21,7 +21,14 @@
                     </div>
 
                     <div class="border-t border-gray-200 pt-4 space-y-3">
-
+<div>
+                            <span class="block text-sm font-medium text-gray-600">
+                                Image
+                            </span>
+                            <span class="rounded-lg border">
+                                <img src="{{$employant->pfp }}" alt="pfp">
+                            </span>
+                        </div>
                         <div>
                             <span class="block text-sm font-medium text-gray-600">
                                 Nom
